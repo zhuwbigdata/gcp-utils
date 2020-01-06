02cd9781c311a93db25d8b54280dc659f3cc15e1
wayne zhu <zhuw.bigdata@gmail.com>
2020-01-06 11:27:31 -0600

    More on DM

--- sdk/bin/deploymentmanager/dmDeploymentCreatePreview.sh
+++ sdk/bin/deploymentmanager/dmDeploymentCreatePreview.sh
@@ -4,9 +4,8 @@ check_usage() {
   then
     echo "Usage:"
     echo "$0 <Deployment_Name> <Template_File>""
-    echo "Needs to update after preview ..."
     exit 1
   fi
 }
 check_usage $*
-gcloud deployment-manager deployments update $1 --config=$2 
+gcloud deployment-manager deployments create $1 --config=$2 --preview
